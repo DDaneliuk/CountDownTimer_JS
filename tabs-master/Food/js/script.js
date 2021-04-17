a9474c0a455dcacde8393c3e9c45555acdad0d1f
Denchik Danelyuk <ddanelyuk@e1r6p8.unit.ua>
2021-04-17 17:24:10 +0300

    Add modal window

--- tabs-master/Food/js/script.js
+++ tabs-master/Food/js/script.js
@@ -7,8 +7,6 @@ window.addEventListener("DOMContentLoaded", () => {
   let tabsContent = document.querySelectorAll(".tabcontent");
   let tabsParent = document.querySelector(".tabheader__items");
 
-  console.log(tabs);
-
   function hideTabContent() {
     tabsContent.forEach((item) => {
       item.style.display = "none";
@@ -40,7 +38,7 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 
   //Timer
-  const deadline = "2021-03-30";
+  const deadline = "2021-06-30";
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
@@ -88,4 +86,38 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
   setClock(".timer", deadline);
+
+  //Modal window
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+        modalTrigger.forEach(btn => {
+          btn.addEventListener('click', function() {
+              modal.classList.add('show');
+              modal.classList.remove('hide');
+              // Либо вариант с toggle - но тогда назначить класс в верстке
+              document.body.style.overflow = 'hidden';
+          });
+      });
+
+    function closeModal(){
+      modal.classList.remove('show');
+      modal.classList.add('hide');
+      document.body.style.overflow = '';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) =>{
+      if(e.target === modal){
+        closeModal();
+      }
+    })
+    document.addEventListener('keydown', (e) =>{
+      if(e.code === "Escape" && modal.classList.contains('show')){
+        closeModal();
+      }
+    })
 });
